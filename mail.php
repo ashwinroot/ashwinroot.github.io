a38ca2e3c9b0b59a33ceed470313105c710023c8
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2019-01-10 18:59:52 +0530

    Minor modifications

--- mail.php
+++ mail.php
@@ -1,10 +1,10 @@
 <?php
 
-$msg = "Call request to attend at ";
+$msg = "Reminder alert: Daily updates ";
 $msg = wordwrap($msg,70);
 $headers = 'From: ashwinoscar@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-$p = mail('ashwinoscar@gmail.com','Alert',$msg,$headers);
+$p = mail('ashwinoscar@gmail.com','See whats new',$msg,$headers);
 echo $p;
  ?>
